754df2128a4cb60df06356d41b23c384ac351711
Acalltoredemption <acalltoredemption@gmail.com>
2020-12-12 18:24:37 -0800

    began setting up StreamCreate component and integrating form validation with redux-form

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -1,12 +1,46 @@
 import React from 'react';
+import { Field, reduxForm } from 'redux-form';
 
 
-const StreamCreate = () => {
-    return <div>StreamCreate</div>;
+
+class StreamCreate extends React.Component {
+renderInput({input, label}){
+return (
+    <div className="field">
+        <label>{label}</label>
+<input {...input} />
+</div>
+);
+}
+
+onSubmit(formValues) {
+console.log(formValues);
 
 }
 
+    render() {
+        return (
+            <form onSubmit={this.props.handleSubmit(this.onSubmit)} className="ui form">
+                <Field label="Enter Title" name="title" component={this.renderInput} />
+                <Field label="Enter Description" name="description" component={this.renderInput} />
+                <button className="ui button primary">Submit</button>
+            </form>
+        );
+    }
+
+}
+
+const validate = (formValues) => {
+    const errors = {};
+if(!formValues.tltle){
+    errors.title = 'You must enter a title!';
+} if (!formValues.description){
+    errors.description = 'You must enter a description!';
+}
+return errors;
+};
 
 
-
-export default StreamCreate;
+export default reduxForm({
+    form: 'streamCreate'
+})(StreamCreate);
