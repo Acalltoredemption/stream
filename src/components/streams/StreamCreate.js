24038db62a2a35bcec0f5f6d3fc6337cbe8a3573
Acalltoredemption <acalltoredemption@gmail.com>
2020-12-13 19:15:36 -0800

    added in a bunch of action creators for our future functionality

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Field, reduxForm } from 'redux-form';
-
+import { connect } from 'react-redux';
+import { createStream } from '../../actions';
 
 
 class StreamCreate extends React.Component {
@@ -26,8 +27,8 @@ return (
 );
 }
 
-onSubmit(formValues) {
-console.log(formValues);
+onSubmit = (formValues) => {
+this.props.createStream(formValues);
 
 }
 
@@ -54,7 +55,9 @@ return errors;
 };
 
 
-export default reduxForm({
+const formWrapped = reduxForm({
     form: 'streamCreate',
     validate: validate
 })(StreamCreate);
+
+export default connect(null, { createStream })(formWrapped);
