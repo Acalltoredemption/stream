b71b0ab6bc664ecf6a077e081caa2d7552bef7e2
Acalltoredemption <acalltoredemption@gmail.com>
2020-12-15 16:53:53 -0800

    framework of project more or less done.. will integrate streaming portion next

--- src/components/streams/StreamList.js
+++ src/components/streams/StreamList.js
@@ -31,7 +31,9 @@ return(
         {this.renderAdmin(stream)}
         <i className="large middle aligned icon camera" />
         <div className="content">
+            <Link to={`/streams/${stream.id}`} className="header">
             {stream.title}
+            </Link>
             <div className="description">{stream.description}</div>
         </div>
         
