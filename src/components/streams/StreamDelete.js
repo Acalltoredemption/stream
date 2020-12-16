5340c5dfb1d8819d4107561f654029491fa61d91
Acalltoredemption <acalltoredemption@gmail.com>
2020-12-15 16:02:38 -0800

    adding in stream props to our StreamDelete component so we can dynamically display stream title in delete modal

--- src/components/streams/StreamDelete.js
+++ src/components/streams/StreamDelete.js
@@ -1,27 +1,52 @@
 import React from 'react';
 import Modal from '../Modal';
+import history from '../../history';
+import { connect } from 'react-redux';
+import { fetchStream } from '../../actions';
 
-const StreamDelete = () => {
-    const actions = (
+
+class StreamDelete extends React.Component {
+    componentDidMount(){
+        this.props.fetchStream(this.props.match.params.id);
+    }
+
+
+    renderActions(){
+    return (
         <div>
             <button className="ui button negative">Delete</button>
             <button className="ui button">Cancel</button>
         </div>
     );
+    };
+
+    renderContent(){
+        if(!this.props.stream){
+            return 'Are you sure you want to delete this stream?'
+        }
+
+        return `Are you sure you want to delete the stream with title: ${this.props.stream.title} `
+    }
+
+
+    render(){
     return (
-    <div>
-        StreamDelete
+
+
         <Modal
          header="Stream Delete"
-         content="Are you sure you want to delete this stream?"
-         actions={actions}
+         content={this.renderContent()}
+         actions={this.renderActions()}
+         onDismiss={() => history.push('/')}
          />
-        </div>
+        
     );
-
+    }
 }
 
+const mapStateToProps = (state, ownProps) => {
+    return { stream: state.streams[ownProps.match.params.id] }
+};
 
 
-
-export default StreamDelete;
+export default connect(mapStateToProps, { fetchStream })(StreamDelete);
